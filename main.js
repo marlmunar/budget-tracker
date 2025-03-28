9f61e17a0e1f87bab33b92b2a3d4b0c3bacee2b7
marlmunar <marlemmmunar@gmail.com>
2025-03-28 13:15:52 +0800

    Added filter by category feature

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ document.getElementById("expenseForm").addEventListener("submit", (e) => {
   let amount = parseInt(document.getElementById("amount").value.trim());
   let category = document.getElementById("category").value.trim();
   trackingList.addEntry(expense, amount, category);
-  updateDOM();
+  updateDOM(trackingList.list);
   clearForm();
 });
 
 document.getElementById("expenseForm").addEventListener("reset", clearForm());
 
-function updateDOM() {
-  updateLog();
+function updateDOM(list) {
+  updateLog(list);
   updateSummary();
   toggleHeaders();
 }
@@ -47,19 +47,27 @@ const trackingList = {
     console.log(this.list);
   },
 
+  filterBy(categories) {
+    return Object.values(this.list).filter((item) =>
+      categories.includes(item.category)
+    );
+  },
+
+  uniqueCategories() {
+    return [...new Set(Object.values(this.list).map((item) => item.category))];
+  },
+
   getTotal() {
-    const total = Object.values(this.list).reduce(
+    return Object.values(this.list).reduce(
       (sum, item) => +sum + +item.amount,
       0
     );
-    return total;
   },
 
   sumByCategory(category) {
-    const sum = Object.values(this.list)
+    return Object.values(this.list)
       .filter((item) => item.category === category)
       .reduce((sum, item) => +sum + +item.amount, 0);
-    return sum;
   },
 
   isNotEmpty() {
@@ -72,20 +80,20 @@ const trackingList = {
   },
 };
 
-function updateLog() {
+function updateLog(list) {
   console.log("Updating log");
-  console.log(Object.keys(trackingList.list)[0]);
+  console.log(list);
   const log = document.getElementById("log");
-  if (trackingList.isNotEmpty()) {
+  if (Object.keys(list).length > 0) {
     log.innerHTML = "";
-    for (let id in trackingList.list) {
+    for (let id in list) {
       log.insertAdjacentHTML(
         "beforeend",
         `
             <li class="expense-item" id="${id}">
-                    <span class="expense-name">${trackingList.list[id].expense}</span>
-                    <span class="espense-amount">${trackingList.list[id].amount}</span>
-                    <span class="expense-category">${trackingList.list[id].category}</span>
+                    <span class="expense-name">${list[id].expense}</span>
+                    <span class="espense-amount">${list[id].amount}</span>
+                    <span class="expense-category">${list[id].category}</span>
                     <button id="edit">Edit</button>
                     <button id="delete">Delete</button>
                   </li>
@@ -127,7 +135,7 @@ document.getElementById("update").addEventListener("submit", (e) => {
     let amount = document.getElementById("updateAmount").value.trim();
     let category = document.getElementById("updateCategory").value.trim();
     trackingList.editEntry(id, expense, amount, category);
-    updateDOM();
+    updateDOM(trackingList.list);
     closeUpdateForm();
     const edited = document.getElementById(id);
     edited.scrollIntoView({ behavior: "smooth" });
@@ -137,7 +145,7 @@ document.getElementById("update").addEventListener("submit", (e) => {
 
 function deleteEntry(id) {
   trackingList.removeEntry(id);
-  updateDOM();
+  updateDOM(trackingList.list);
 }
 
 function updateEntry(id) {
@@ -147,7 +155,7 @@ function updateEntry(id) {
   const log = document.getElementById("update");
   log.insertAdjacentHTML(
     "afterBegin",
-    `<form id="updateForm" class="update-entry">
+    `<form id="updateForm" class="update-entry absolute-box">
               <div id="${id}" class="section-title"><h3>Edit entry</h3></div>
               <div>
                 <div>
@@ -200,22 +208,20 @@ function updateEntry(id) {
 }
 
 function closeUpdateForm() {
-  const updateBox = document.getElementById("updateForm");
-  updateBox.remove();
+  const updateForm = document.getElementById("updateForm");
+  updateForm.remove();
 }
 
 function updateSummary() {
   const summary = document.getElementById("summary");
   const total = document.getElementById("total");
   console.log("updating summary");
-  const uniqueCategories = [
-    ...new Set(Object.values(trackingList.list).map((item) => item.category)),
-  ];
+  const categories = trackingList.uniqueCategories();
   console.log("unique categories");
-  console.log(uniqueCategories);
+  console.log(categories);
   if (trackingList.isNotEmpty()) {
     summary.innerHTML = "";
-    uniqueCategories.sort().forEach((category) => {
+    categories.sort().forEach((category) => {
       summary.insertAdjacentHTML(
         "afterbegin",
         `
@@ -249,4 +255,75 @@ function toggleHeaders() {
   headers.forEach((el) => {
     el.style.display = "none";
   });
+}
+
+document.getElementById("filterBox").addEventListener("click", (e) => {
+  if (e.target.tagName !== "BUTTON") {
+    return;
+  }
+  e.target.matches("#cancel") && closeFilterForm();
+});
+
+document.getElementById("filter").addEventListener("click", (e) => {
+  console.log("Open filter");
+  const filterBox = document.getElementById("filterBox");
+  const uniqueCategories = trackingList.uniqueCategories();
+
+  filterBox.insertAdjacentHTML(
+    "afterBegin",
+    `<form id="filterForm" class="absolute-box filter-table">
+      <div class="section-title">
+        <h3>Filter by Categories</h3>
+      </div>
+      <div>
+        <div class="checklist">
+          ${uniqueCategories
+            .sort()
+            .map(
+              (category, index) => `
+              <div class="checkbox">
+                <input type="checkbox" id="category${
+                  index + 1
+                }" name="${category}" checked/>
+                <label for="category${index + 1}">${category}</label>
+              </div>
+            `
+            )
+            .join("")}
+        </div>
+        <div class="button-row">
+          <button type="submit">Save</button>
+          <button type="button" id="cancel">Cancel</button>
+        </div>
+      </div>
+    </form>`
+  );
+});
+
+document.getElementById("filterBox").addEventListener("submit", (e) => {
+  if (e.target.matches("#filterForm")) {
+    e.preventDefault();
+
+    const filterBox = document.getElementById("filterBox");
+    const selectedItems = filterBox.querySelectorAll(
+      'input[type="checkbox"]:checked'
+    );
+    const categories = Array.from(selectedItems).map((item) => item.name);
+    console.log(trackingList.filterBy(categories));
+    const tempList = trackingList.filterBy(categories);
+    // let id = document.getElementById("updateForm").firstElementChild.id;
+    // console.log(`Saving updates for ${id}`);
+    // let expense = document.getElementById("updateExpense").value.trim();
+    // let amount = document.getElementById("updateAmount").value.trim();
+    // let category = document.getElementById("updateCategory").value.trim();
+    // trackingList.editEntry(id, expense, amount, category);
+    // updateDOM();
+    updateDOM(tempList);
+    closeFilterForm();
+  }
+});
+
+function closeFilterForm() {
+  const filterForm = document.getElementById("filterForm");
+  filterForm.remove();
 }
