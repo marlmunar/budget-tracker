63837e59ab181b721df427e30938b5df1da3c126
marlmunar <marlemmmunar@gmail.com>
2025-03-31 13:04:36 +0800

    Edited the trackinglist to accomodate array type of list instead

--- refactor.js
+++ refactor.js
@@ -1,59 +1,45 @@
 const trackingList = {
-  id: 0,
-  list: {},
+  list: [],
 
   addEntry(expense, amount, category) {
-    this.id++;
-    this.list[this.id] = { expense, amount, category };
+    this.list.push({ id: Date.now(), expense, amount, category });
   },
 
   removeEntry(id) {
-    console.log("deleting entry");
-    console.log(" entry");
-    console.log(this.list[id]);
-    delete this.list[id];
-    console.log(this.list);
+    this.list = this.list.filter((entry) => entry.id !== id);
   },
 
   editEntry(id, expense, amount, category) {
-    console.log("updating entry");
-    console.log("entry");
-    console.log(this.list[id]);
-    this.list[id] = { expense, amount, category };
-    console.log(this.list);
+    this.list = this.list.map((entry) =>
+      entry.id === id ? { ...entry, expense, amount, category } : entry
+    );
   },
 
   filterBy(categories) {
-    return Object.values(this.list).filter((item) =>
-      categories.includes(item.category)
-    );
+    return this.list.filter((item) => categories.includes(item.category));
   },
 
-  selectCategories() {},
+  selectedCategories() {},
 
   uniqueCategories() {
-    return [...new Set(Object.values(this.list).map((item) => item.category))];
+    return this.list.map((item) => item.category);
   },
 
   getTotal() {
-    return Object.values(this.list).reduce(
-      (sum, item) => +sum + +item.amount,
-      0
-    );
+    return this.list.reduce((sum, item) => +sum + +item.amount, 0);
   },
 
   sumByCategory(category) {
-    return Object.values(this.list)
+    return this.list
       .filter((item) => item.category === category)
       .reduce((sum, item) => +sum + +item.amount, 0);
   },
 
   isNotEmpty() {
-    return Object.keys(this.list).length > 0;
+    return this.list.length > 0;
   },
 
   reset() {
-    this.id = 0;
-    this.list = {};
+    this.list = [];
   },
 };
