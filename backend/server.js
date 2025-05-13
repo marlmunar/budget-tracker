a4634c9156a9c90543c4b792984619fc9c8a7b7f
marlmunar <marlemmmunar@gmail.com>
2025-05-13 08:26:08 +0800

    Connected the routes to the server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/user.routes";
+import logRoutes from "./routes/log.routes";
+
 dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
+app.use("/api/users/", userRoutes);
+app.use("/api/logs", logRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
