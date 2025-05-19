a4ac5a96f609eeddd02736d4131dbc04914c6f56
marlmunar <marlemmmunar@gmail.com>
2025-05-19 10:39:03 +0800

    Added sign up and log in button to the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,13 @@
 import { RxDotsHorizontal } from "react-icons/rx";
 import { BsPersonSquare } from "react-icons/bs";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { IoMdArrowDropup } from "react-icons/io";
+import { useState } from "react";
 
 const Header = () => {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <header className="header">
       <div>
@@ -9,33 +15,66 @@ const Header = () => {
       </div>
 
       <div>
-        <button className="hamburger">
-          <RxDotsHorizontal className="text-2xl" />
-        </button>
-        <div className="profile">
-          <button>
-            <BsPersonSquare className="text-3xl" />
-          </button>
-          <p>
-            Hello, <span className="font-bold">Current User!</span>
-          </p>
-        </div>
+        {isLoggedIn ? (
+          <>
+            <button
+              className="hamburger"
+              onClick={() => setIsClicked((i) => !i)}
+            >
+              <RxDotsHorizontal className="text-2xl" />
+            </button>
+            <div className="profile">
+              <button>
+                <BsPersonSquare className="text-2xl mr-1" />
+              </button>
+              <p>
+                Hello, <span className="font-bold">Current User!</span>
+              </p>
+              <button
+                onClick={() => {
+                  setIsClicked((i) => !i);
+                }}
+              >
+                {isClicked ? (
+                  <IoMdArrowDropup className="icon" />
+                ) : (
+                  <IoMdArrowDropdown className="icon" />
+                )}
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="login">
+            <button className="login-button">Login</button>
+            <button className="login-button hidden md:block">Sign Up</button>
+          </div>
+        )}
 
-        <nav className="nav-bar">
-          <ul>
-            <li>
-              <a className="nav-active" href="/">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/profile">Profile</a>
-            </li>
-            <li>
-              <a href="/logout">Logout</a>
-            </li>
-          </ul>
-        </nav>
+        {isClicked && (
+          <nav className="nav-bar">
+            <ul>
+              <li>
+                <a
+                  className="nav-active"
+                  href="/"
+                  onClick={() => setIsClicked(false)}
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/profile" onClick={() => setIsClicked(false)}>
+                  Profile
+                </a>
+              </li>
+              <li>
+                <a href="/logout" onClick={() => setIsClicked(false)}>
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </nav>
+        )}
       </div>
     </header>
   );
