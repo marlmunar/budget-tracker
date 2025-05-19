182b8930004ba184aa85e2f4af159f164df5c63f
marlmunar <marlemmmunar@gmail.com>
2025-05-19 13:47:29 +0800

    Added a footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 const App = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
       <div className="container mx-auto px-4 xl:px-10">
         <Home />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="mt-auto">
+      <div className="bg-amber-400">
+        <div className="container  mx-auto px-10 min-h-10 flex items-center justify-between">
+          <p>
+            Copyright &#169; 2025{" "}
+            <span className="font-semibold"> Budgetarians' Log</span>
+          </p>
+          <p>
+            Project by <span className="font-semibold">marl.dev</span>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
