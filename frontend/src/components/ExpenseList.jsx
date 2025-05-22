83e55d8614b02158af2b1c7d45fdf28b7f8b547e
marlmunar <marlemmmunar@gmail.com>
2025-05-22 10:23:53 +0800

    Added a no records indicator component

--- frontend/src/components/ExpenseList.jsx
+++ frontend/src/components/ExpenseList.jsx
@@ -1,19 +1,11 @@
+import NoRecords from "./NoRecords";
+
 const ExpenseList = () => {
   return (
     <section className="log-section-container">
       <h2 className="log-section-header">Expense List</h2>
 
-      <ul>
-        {/* <div className="hidden">
-          <li className="header-row log">
-            <span className="expense-name">Expense</span>
-            <span className="espense-amount">Amount</span>
-            <span className="expense-category">Category</span>
-            <span className="">Options</span>
-          </li>
-        </div> */}
-        <div id="update"></div>
-      </ul>
+      <NoRecords />
     </section>
   );
 };
---
+++ frontend/src/components/NoRecords.jsx
@@ -0,0 +1,9 @@
+const NoRecords = () => {
+  return (
+    <div className="flex justify-center items-center min-h-20">
+      <p>No Records Yet</p>
+    </div>
+  );
+};
+
+export default NoRecords;
