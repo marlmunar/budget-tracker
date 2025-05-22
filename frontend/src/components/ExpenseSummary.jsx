e3435a53a3ed2ccbe560330d57547d4f5e8c2ca9
marlmunar <marlemmmunar@gmail.com>
2025-05-22 10:26:45 +0800

    Added the expense summary component: created the container only

---
+++ frontend/src/components/ExpenseSummary.jsx
@@ -0,0 +1,13 @@
+import NoRecords from "./NoRecords";
+
+const ExpenseSummary = () => {
+  return (
+    <section className="log-section-container">
+      <h2 className="log-section-header">Expense Summary</h2>
+
+      <NoRecords />
+    </section>
+  );
+};
+
+export default ExpenseSummary;
--- frontend/src/pages/LogScreen.jsx
+++ frontend/src/pages/LogScreen.jsx
@@ -1,38 +1,18 @@
 import React from "react";
 import AddEntryForm from "../components/AddEntryForm";
 import ExpenseList from "../components/ExpenseList";
+import ExpenseSummary from "../components/ExpenseSummary";
 
 const LogScreen = () => {
   return (
-    <div>
+    <main>
       <h2 className="text-2xl font-semibold underline">Log Name</h2>
       <div className="grid grid-cols-[35%_65%] grid-rows-2 my-2">
         <AddEntryForm />
         <ExpenseList />
-        <section id="expense-summary">
-          <div className="section-title">
-            <h2>Expense Summary</h2>
-          </div>
-          <ul>
-            <div className="hidden">
-              <li className="header-row summary">
-                <span className="expense-name">Category</span>
-                <span className="espense-amount">Total</span>
-              </li>
-            </div>
-            <div id="summary">
-              <div className="no-data">No Data</div>
-            </div>
-            <div className="hidden">
-              <li className="per-category total">
-                <span>Total amount spent</span>
-                <span id="total"></span>
-              </li>
-            </div>
-          </ul>
-        </section>
+        <ExpenseSummary />
       </div>
-    </div>
+    </main>
   );
 };
 
