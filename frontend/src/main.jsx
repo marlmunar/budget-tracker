fada3e33c71a9be5eedfe2497ec6d2701d38d2c7
marlmunar <marlemmmunar@gmail.com>
2025-05-23 09:36:12 +0800

    Added a privateRoute to protect routes that needs authentication to access

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,16 +17,19 @@ import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound.jsx";
 import LogScreen from "./pages/LogScreen.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/log" element={<LogScreen />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/*" element={<NotFound />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="log" element={<LogScreen />} />
+        <Route path="profile" element={<Profile />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
