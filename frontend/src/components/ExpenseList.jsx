5cdfe34c5fad11a5be09baaa2480f760280fae0e
marlmunar <marlemmmunar@gmail.com>
2025-05-27 22:04:36 +0800

    Added timestamps on the expense list items

--- frontend/src/components/ExpenseList.jsx
+++ frontend/src/components/ExpenseList.jsx
@@ -7,9 +7,24 @@ const ExpenseList = () => {
       <h2 className="log-section-header">Expense List</h2>
 
       <NoRecords />
-      <ExpenseListItem name="Food 1" amount="300" category="Needs" />
-      <ExpenseListItem name="Savings 2" amount="40000" category="Savings" />
-      <ExpenseListItem name="Savings 3" amount="40000" category="Savings" />
+      <ExpenseListItem
+        name="Food 1"
+        amount="300"
+        category="Needs"
+        timeStamps="05/07/2025"
+      />
+      <ExpenseListItem
+        name="Savings 2"
+        amount="40000"
+        category="Savings"
+        timeStamps="05/05/2025"
+      />
+      <ExpenseListItem
+        name="Savings 1"
+        amount="40000"
+        category="Savings"
+        timeStamps="05/01/2025"
+      />
     </section>
   );
 };
--- frontend/src/components/ExpenseListItem.jsx
+++ frontend/src/components/ExpenseListItem.jsx
@@ -1,11 +1,14 @@
 import { TbTrash, TbEdit } from "react-icons/tb";
-const ExpenseListItem = ({ name, amount, category }) => {
+const ExpenseListItem = ({ name, amount, category, timeStamps }) => {
   return (
     <div className="my-2 bg-white shadow rounded px-4 p-2 grid grid-rows-2 grid-cols-3 items-center hover:bg-slate-200/50 transition-all duration-300">
-      <p className="text-lg font-semibold col-span-2">{name}</p>
+      <div className="col-span-2">
+        <p className="text-lg font-semibold my-1">{name}</p>
+        <p className="text-xs">{timeStamps}</p>
+      </div>
       <p>{amount}</p>
-      <p>{category}</p>
-      <div className="flex gap-2 justify-end row-start-1 col-start-3 row-span-2">
+      <p className="">{category}</p>
+      <div className="py-2 flex gap-2 justify-end row-start-1 col-start-3 row-span-2 self-start">
         <TbEdit className="rounded text-3xl p-1 border-2 md:min-w-10 bg-white" />
         <TbTrash className="rounded text-3xl p-1 border-2 md:min-w-10 bg-white" />
       </div>
