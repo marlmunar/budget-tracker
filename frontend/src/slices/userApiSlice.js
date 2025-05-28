af8dc0415591848e532a3e406b5aa65b39d4b6ab
marlmunar <marlemmmunar@gmail.com>
2025-05-28 20:50:28 +0800

    Added the userApiSlice with login and register mutations

---
+++ frontend/src/slices/userApiSlice.js
@@ -0,0 +1,23 @@
+import { apiSlice } from "./apiSlice";
+const USER_URL = "/api/user";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/login`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USER_URL}/register`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useRegisterMutation } = usersApiSlice;
