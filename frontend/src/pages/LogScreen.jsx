cfe7d11dfabfd8ac46742c4ace960587b5f54b6b
marlmunar <marlemmmunar@gmail.com>
2025-05-30 11:08:00 +0800

    Edited the buttons for the log options in log screen and added a save button

--- frontend/src/pages/LogScreen.jsx
+++ frontend/src/pages/LogScreen.jsx
@@ -2,7 +2,14 @@ import { useState } from "react";
 import AddEntryForm from "../components/AddEntryForm";
 import ExpenseList from "../components/ExpenseList";
 import ExpenseSummary from "../components/ExpenseSummary";
-import { TbDots } from "react-icons/tb";
+import {
+  TbFileDots,
+  TbDeviceSdCard,
+  TbFilePencil,
+  TbFileDownload,
+  TbFileAnalytics,
+  TbFileX,
+} from "react-icons/tb";
 
 const LogScreen = () => {
   const [isSelecting, setIsSelecting] = useState(false);
@@ -11,17 +18,33 @@ const LogScreen = () => {
       <title>Budgetarians' Log - Log Name</title>
       <div className="relative flex justify-between items-center">
         <h2 className="text-2xl font-semibold underline">Log Name</h2>
-        <TbDots
-          className=" border-2 w-15 rounded text-3xl cursor-pointer hover:bg-slate-300/50 hover:shadow-md hover:border-transparent transition-all duration-300"
-          onClick={() => setIsSelecting((i) => !i)}
-        />
-        {isSelecting && (
-          <div className="text-lg p-2 py-4 absolute shadow border-2 flex flex-col items-center gap-2 w-28 bg-white rounded right-0 top-10">
-            <button className="underline">Rename</button>
-            <button className="underline">Download</button>
-            <button className="underline text-red-500">Delete</button>
-          </div>
-        )}
+        <div className="flex gap-2 text-3xl">
+          <TbDeviceSdCard className="p-1 rounded cursor-pointer w-12 h-10 hover:bg-slate-300/50 hover:shadow-md hover:border-transparent transition-all duration-300" />
+          <TbFileDots
+            className="p-1 rounded cursor-pointer w-12 h-10 hover:bg-slate-300/50 hover:shadow-md hover:border-transparent transition-all duration-300"
+            onClick={() => setIsSelecting((i) => !i)}
+          />
+          {isSelecting && (
+            <div className="text-base mt-14 p-2 absolute shadow border-2 flex flex-col items-center gap-2 w-32 bg-white rounded right-0 topx-10">
+              <div className="log-options">
+                <TbFilePencil />
+                <span>Rename</span>
+              </div>
+              <div className="log-options">
+                <TbFileDownload />
+                <span>Download</span>
+              </div>
+              <div className="log-options">
+                <TbFileAnalytics />
+                <span>Visualize</span>
+              </div>
+              <div className="log-options text-red-500">
+                <TbFileX />
+                <span>Delete</span>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
 
       <div className="py-4 grid grid-cols-1 justify-between lg:grid-cols-[39%_59%] grid-rows-[min-content_min-content] my-2 gap-4">
