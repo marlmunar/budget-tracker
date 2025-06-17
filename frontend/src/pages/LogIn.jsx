b08b6b90f277eea0ed121af3d785f9f3dc3dceb2
marlmunar <marlemmmunar@gmail.com>
2025-06-17 10:59:40 +0800

    Added transition between login and register pages

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,16 +1,18 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import FormContainer from "../components/FormContainer";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setIsLoggingIn } from "../slices/userSlice";
 import { useLoginMutation } from "../slices/userApiSlice";
 import { setCredentials } from "../slices/authSlice";
+import { AnimatePresence, motion } from "framer-motion";
 const loginChannel = new BroadcastChannel("login_channel");
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [isVisible, setIsVisible] = useState(true);
 
   const [login, { isLoading }] = useLoginMutation();
 
@@ -51,49 +53,67 @@ const Login = () => {
   return (
     <main>
       <title>Budgetarians' Log - Login</title>
-      <FormContainer>
-        <form onSubmit={handleSubmit} className="form-body">
-          <h3 className="text-2xl font-semibold underline">Log In</h3>
+      <AnimatePresence mode="wait">
+        {isVisible && (
+          <motion.div
+            key="login"
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -100 }}
+            transition={{ duration: 0.5 }}
+          >
+            <FormContainer>
+              <form onSubmit={handleSubmit} className="form-body">
+                <h3 className="text-2xl font-semibold underline">Log In</h3>
 
-          <div className="form-input-container">
-            <label htmlFor="email">Email</label>
-            <input
-              className="form-input"
-              type="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              autoComplete="off"
-              required
-            />
-          </div>
-          <div className="form-input-container">
-            <label htmlFor="password">Password</label>
-            <input
-              className="form-input"
-              type="password"
-              name="password"
-              autoComplete="off"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
+                <div className="form-input-container">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    className="form-input"
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    autoComplete="off"
+                    required
+                  />
+                </div>
+                <div className="form-input-container">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    className="form-input"
+                    type="password"
+                    name="password"
+                    autoComplete="off"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
 
-          <div className="pl-1 text-red-500 text-sm">{error}</div>
+                <div className="pl-1 text-red-500 text-sm">{error}</div>
 
-          <button type="submit" formNoValidate className="form-button">
-            Log In
-          </button>
+                <button type="submit" formNoValidate className="form-button">
+                  Log In
+                </button>
 
-          <div className="mt-10">
-            <p>New user?</p>
-            <Link to="/register" className="underline">
-              Sign Up Instead
-            </Link>
-          </div>
-        </form>
-      </FormContainer>
+                <div className="mt-10">
+                  <p>New user?</p>
+                  <span
+                    className="underline cursor-pointer"
+                    onClick={() => {
+                      setIsVisible(false);
+                      setTimeout(() => navigate("/register"), 450);
+                    }}
+                  >
+                    Sign Up Instead
+                  </span>
+                </div>
+              </form>
+            </FormContainer>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </main>
   );
 };
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,11 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import FormContainer from "../components/FormContainer";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setIsLoggingIn } from "../slices/userSlice";
 import { useRegisterMutation } from "../slices/userApiSlice";
 import { setCredentials } from "../slices/authSlice";
+import { AnimatePresence, motion } from "framer-motion";
 const loginChannel = new BroadcastChannel("login_channel");
 
 const Register = () => {
@@ -13,6 +14,7 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
+  const [isVisible, setIsVisible] = useState(true);
 
   const [register, { isLoading }] = useRegisterMutation();
 
@@ -64,77 +66,94 @@ const Register = () => {
   return (
     <main>
       <title>Budgetarians' Log - Register</title>
+      <AnimatePresence mode="wait">
+        {isVisible && (
+          <motion.div
+            key="login"
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 100 }}
+            transition={{ duration: 0.5 }}
+          >
+            <FormContainer>
+              <form
+                onSubmit={handleSubmit}
+                className="form-body min-h-[min-content] "
+              >
+                <h3 className="text-2xl font-semibold underline">Sign Up</h3>
 
-      <FormContainer>
-        <form
-          onSubmit={handleSubmit}
-          className="form-body min-h-[min-content] border"
-        >
-          <h3 className="text-2xl font-semibold underline">Sign Up</h3>
+                <div className="form-input-container">
+                  <label htmlFor="name">Name</label>
+                  <input
+                    className="form-input"
+                    type="text"
+                    name="name"
+                    autoComplete="off"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-input-container">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    className="form-input"
+                    type="email"
+                    name="email"
+                    autoComplete="off"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-input-container">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    className="form-input"
+                    type="password"
+                    name="password"
+                    autoComplete="off"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-input-container">
+                  <label htmlFor="confirmPassword">Confirm Password</label>
+                  <input
+                    className="form-input"
+                    type="password"
+                    name="confirmPassword"
+                    autoComplete="off"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                  />
+                </div>
 
-          <div className="form-input-container">
-            <label htmlFor="name">Name</label>
-            <input
-              className="form-input"
-              type="text"
-              name="name"
-              autoComplete="off"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-input-container">
-            <label htmlFor="email">Email</label>
-            <input
-              className="form-input"
-              type="email"
-              name="email"
-              autoComplete="off"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-input-container">
-            <label htmlFor="password">Password</label>
-            <input
-              className="form-input"
-              type="password"
-              name="password"
-              autoComplete="off"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-input-container">
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input
-              className="form-input"
-              type="password"
-              name="confirmPassword"
-              autoComplete="off"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
-          </div>
+                <div className="pl-1 text-red-500 text-sm">{error}</div>
 
-          <div className="pl-1 text-red-500 text-sm">{error}</div>
+                <button type="submit" formNoValidate className="form-button">
+                  Sign Up
+                </button>
 
-          <button type="submit" formNoValidate className="form-button">
-            Sign Up
-          </button>
-
-          <div className="mt-10">
-            <p>Already a user?</p>
-            <Link to="/login" className="underline">
-              Log In Instead
-            </Link>
-          </div>
-        </form>
-      </FormContainer>
+                <div className="mt-10">
+                  <p>Already a user?</p>
+                  <span
+                    className="underline cursor-pointer"
+                    onClick={() => {
+                      setIsVisible(false);
+                      setTimeout(() => navigate("/login"), 450);
+                    }}
+                  >
+                    Log In Instead
+                  </span>
+                </div>
+              </form>
+            </FormContainer>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </main>
   );
 };
