36653a9922d47f9d6a2d5d3a499a862c8cf249fd
marlmunar <marlemmmunar@gmail.com>
2025-06-17 15:26:38 +0800

    Added transition to calendar component

--- frontend/src/components/Visualize.jsx
+++ frontend/src/components/Visualize.jsx
@@ -4,6 +4,7 @@ import { useLazyGetLogQuery } from "../slices/logsApiSlice";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { startLoading, stopLoading } from "../slices/appSlice";
 import { TbArrowBackUp } from "react-icons/tb";
+import { motion } from "framer-motion";
 const days = [
   "Sunday",
   "Monday",
@@ -42,6 +43,7 @@ const Visualize = () => {
 
   const [month, setMonth] = useState(new Date().getMonth());
   const [year, setYear] = useState(new Date().getFullYear());
+  const [startX, setStartX] = useState(0);
   const [width, setWidth] = useState(0);
   const parentRef = useRef(null);
   const present = new Date();
@@ -156,6 +158,7 @@ const Visualize = () => {
         setYear((prev) => prev - 1);
       } else {
         setMonth((prev) => prev - 1);
+        setStartX(-30);
       }
     } else if (direction === "next") {
       if (month === 11) {
@@ -163,6 +166,7 @@ const Visualize = () => {
         setYear((prev) => prev + 1);
       } else {
         setMonth((prev) => prev + 1);
+        setStartX(30);
       }
     }
   };
@@ -172,136 +176,145 @@ const Visualize = () => {
       <title>{`Budgetarians' Log ${
         logData.name ? `- ${logData.name}` : ""
       }`}</title>
-      <div className="lg:flex mb-2 gap-2 text-3xl justify-start w-full">
-        <div className="flex items-center max-h-[min-content]">
-          <Link className="log-button" to={`/log/${logId}`}>
-            <TbArrowBackUp />
-          </Link>
+      <motion.div
+        key={month}
+        initial={{ opacity: 0, x: startX }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.4 }}
+      >
+        <div className="lg:flex mb-2 gap-2 text-3xl justify-start w-full">
+          <div className="flex items-center max-h-[min-content]">
+            <Link className="log-button" to={`/log/${logId}`}>
+              <TbArrowBackUp />
+            </Link>
 
-          <h2 className="text-2xl font-semibold underline">{logData.name}</h2>
-        </div>
+            <h2 className="text-2xl font-semibold underline">{logData.name}</h2>
+          </div>
 
-        <div className="ml-auto shadow min-h-30 lg:max-w-[65%] grow p-4 rounded mb-2">
-          <h2 className="text-xl font-semibold">Summary</h2>
-          {!!total ? (
-            <div className="flex flex-col lg:flex-row">
-              <div className="text-sm">
-                <h2 className="text-base font-semibold">Ranking</h2>
-                {[...summary]
-                  .sort((a, b) => b.total - a.total)
-                  .map((entry, index) => (
-                    <div
-                      key={index}
-                      className="flex justify-between items-center gap-2 mb-1 w-36"
-                    >
-                      <div className="flex justify-between items-center gap-2">
-                        <div
-                          className="h-3 w-3 rounded-xl shadow"
-                          style={{ backgroundColor: entry.category.color }}
-                        ></div>
-                        <div>{entry.category.name}</div>
+          <div className="ml-auto shadow min-h-30 lg:max-w-[65%] grow p-4 rounded mb-2">
+            <h2 className="text-xl font-semibold">Summary</h2>
+            {!!total ? (
+              <div className="flex flex-col lg:flex-row">
+                <div className="text-sm">
+                  <h2 className="text-base font-semibold">Ranking</h2>
+                  {[...summary]
+                    .sort((a, b) => b.total - a.total)
+                    .map((entry, index) => (
+                      <div
+                        key={index}
+                        className="flex justify-between items-center gap-2 mb-1 w-36"
+                      >
+                        <div className="flex justify-between items-center gap-2">
+                          <div
+                            className="h-3 w-3 rounded-xl shadow"
+                            style={{ backgroundColor: entry.category.color }}
+                          ></div>
+                          <div>{entry.category.name}</div>
+                        </div>
+
+                        <div>{index + 1}</div>
                       </div>
-
-                      <div>{index + 1}</div>
-                    </div>
-                  ))}
-              </div>
-              <div className="ml-auto min-w-[75%] flex flex-col justify-end items-end mb-2">
-                <h2 className="text-2xl font-semibold mb-2">{`Total: ${total}`}</h2>
-                <div
-                  ref={parentRef}
-                  className="ml-auto h-5 rounded-xl overflow-hidden grid grid-rows-1 grid-cols-[repeat(auto-fill,min-content)] grid-flow-col auto-cols-auto w-full max-w-150"
-                >
-                  {summary.map((entry, index) => (
-                    <div
-                      key={index}
-                      style={{
-                        backgroundColor: entry.category.color,
-                        width: (entry.total / total) * width,
-                      }}
-                    ></div>
-                  ))}
+                    ))}
+                </div>
+                <div className="ml-auto min-w-[75%] flex flex-col justify-end items-end mb-2">
+                  <h2 className="text-2xl font-semibold mb-2">{`Total: ${total}`}</h2>
+                  <div
+                    ref={parentRef}
+                    className="ml-auto h-5 rounded-xl overflow-hidden grid grid-rows-1 grid-cols-[repeat(auto-fill,min-content)] grid-flow-col auto-cols-auto w-full max-w-150"
+                  >
+                    {summary.map((entry, index) => (
+                      <div
+                        key={index}
+                        style={{
+                          backgroundColor: entry.category.color,
+                          width: (entry.total / total) * width,
+                        }}
+                      ></div>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          ) : (
-            <div className="text-base">No data for this month</div>
-          )}
-        </div>
-      </div>
-
-      <div className="border-2 min-h-[100%] flex flex-col gap-1 rounded shadow-lg p-2 ">
-        <div className="flex text-xl justify-between items-center h-12 shadow bg-slate-400 rounded p-2">
-          <button
-            className="bg-white rounded lg:min-w-[12rem] px-2 p-1 font-semibold flex justify-center items-center"
-            onClick={() => changeMonth("prev")}
-          >
-            {"< PREV"}
-          </button>
-          <div className="bg-white rounded lg:min-w-[12rem] px-2 p-1 font-semibold flex justify-center items-center">
-            {`${months[month].toUpperCase()} - ${year}`}
+            ) : (
+              <div className="text-base">No data for this month</div>
+            )}
           </div>
-          <button
-            className="bg-white rounded lg:min-w-[12rem] px-2 p-1 font-semibold flex justify-center items-center"
-            onClick={() => changeMonth("next")}
-          >
-            {"NEXT >"}
-          </button>
         </div>
-        <div className="p-1 grow min-h-[100%] grid grid-cols-7 grid-rows-[3rem_repeat(6,minmax(10rem,1fr))] border-2 rounded gap-1">
-          {days.map((day) => (
-            <div
-              key={day}
-              className="truncate p-2 border flex justify-center items-center font-semibold"
+
+        <div className="border-2 min-h-[100%] flex flex-col gap-1 rounded shadow-lg p-2 ">
+          <div className="flex text-xl justify-between items-center h-12 shadow bg-slate-400 rounded p-2">
+            <button
+              className="bg-white rounded lg:min-w-[12rem] px-2 p-1 font-semibold flex justify-center items-center"
+              onClick={() => changeMonth("prev")}
             >
-              {day}
+              {"< PREV"}
+            </button>
+            <div className="bg-white rounded lg:min-w-[12rem] px-2 p-1 font-semibold flex justify-center items-center">
+              {`${months[month].toUpperCase()} - ${year}`}
             </div>
-          ))}
-          {calendarCells.map((cell, i) => {
-            let styles = "border";
-
-            if (cell.type === "today") {
-              styles = "border-5 border-amber-500";
-            }
-
-            if (cell.type === "filler") {
-              styles = "text-gray-200 opacity-95";
-            }
-
-            return (
+            <button
+              className="bg-white rounded lg:min-w-[12rem] px-2 p-1 font-semibold flex justify-center items-center"
+              onClick={() => changeMonth("next")}
+            >
+              {"NEXT >"}
+            </button>
+          </div>
+          <div className="p-1 grow min-h-[100%] grid grid-cols-7 grid-rows-[3rem_repeat(6,minmax(10rem,1fr))] border-2 rounded gap-1">
+            {days.map((day) => (
               <div
-                key={i}
-                className={`${styles} rounded p-2 flex flex-col lg:grid grid-cols-[11%_88%] items-start`}
+                key={day}
+                className="truncate p-2 border flex justify-center items-center font-semibold"
               >
-                <span>{cell.value}</span>
-
-                {daysWithEntries.includes(cell.value) && (
-                  <div className="bg-white rounded w-full h-full text-xs flex flex-col p-1 gap-1">
-                    {entries.map((entry, index) =>
-                      new Date(entry.date.split("T")[0]).getDate() ===
-                        cell.value && cell.type !== "filler" ? (
-                        <span
-                          key={index}
-                          className="px-2 rounded w-full truncate"
-                          style={{ backgroundColor: entry.category.color }}
-                        >
-                          <span className="font-semibold">{entry.amount}</span>
-                          <span>
-                            {" - "}
-                            {entry.category.name}
-                          </span>
-                        </span>
-                      ) : (
-                        ""
-                      )
-                    )}
-                  </div>
-                )}
+                {day}
               </div>
-            );
-          })}
+            ))}
+            {calendarCells.map((cell, i) => {
+              let styles = "border";
+
+              if (cell.type === "today") {
+                styles = "border-5 border-amber-500";
+              }
+
+              if (cell.type === "filler") {
+                styles = "text-gray-200 opacity-95";
+              }
+
+              return (
+                <div
+                  key={i}
+                  className={`${styles} rounded p-2 flex flex-col lg:grid grid-cols-[11%_88%] items-start`}
+                >
+                  <span>{cell.value}</span>
+
+                  {daysWithEntries.includes(cell.value) && (
+                    <div className="bg-white rounded w-full h-full text-xs flex flex-col p-1 gap-1">
+                      {entries.map((entry, index) =>
+                        new Date(entry.date.split("T")[0]).getDate() ===
+                          cell.value && cell.type !== "filler" ? (
+                          <span
+                            key={index}
+                            className="px-2 rounded w-full truncate"
+                            style={{ backgroundColor: entry.category.color }}
+                          >
+                            <span className="font-semibold">
+                              {entry.amount}
+                            </span>
+                            <span>
+                              {" - "}
+                              {entry.category.name}
+                            </span>
+                          </span>
+                        ) : (
+                          ""
+                        )
+                      )}
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
