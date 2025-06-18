f2bcf85411e4509c41114dc1bfb6a83315883702
marlmunar <marlemmmunar@gmail.com>
2025-06-18 18:11:47 +0800

    Fixed login and register bug that made it uneditable in small screens

--- frontend/src/components/FormContainer.jsx
+++ frontend/src/components/FormContainer.jsx
@@ -1,9 +1,9 @@
 const FormContainer = ({ children }) => {
   return (
-    <div className="form-container">
+    <div className="form-container overflow-hidden bg-gray-50">
       <div className="relative justify-between h-full">
-        <>{children}</>
-        <div className="absolute bottom-0 right-0 z-10">
+        <div className="relative z-10">{children}</div>
+        <div className="absolute bottom-0 right-0">
           <div className="relative overflow-hidden h-[20rem] w-[20rem]">
             <div className="absolute bottom-[-10rem] left-[50%] flex justify-center items-center h-[20rem] w-[20rem]">
               <div className="absolute border-amber-700 border-5 h-[20rem] w-[20rem] rounded-[15rem]"></div>
