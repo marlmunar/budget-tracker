59c61de3e56d49e76bc19c01befd9472eaff2946
marlmunar <marlemmmunar@gmail.com>
2025-06-18 18:23:49 +0800

    Adjusted the font size in instructions part

--- frontend/src/components/Instructions.jsx
+++ frontend/src/components/Instructions.jsx
@@ -11,7 +11,7 @@ const Instructions = () => {
           transition={{ duration: 0.5 }}
         >
           <h3 className="item-title">1. Be a Budgetarian</h3>
-          <p className="pl-2 mb-5">
+          <p className="pl-2 mb-5 text-base">
             Sign up and join our growing budgetarian community.
           </p>
         </motion.div>
@@ -22,7 +22,7 @@ const Instructions = () => {
           transition={{ duration: 0.5 }}
         >
           <h3 className="item-title">2. Create a Tracker</h3>
-          <p className="pl-2 mb-5">
+          <p className="pl-2 mb-5 text-base">
             Open the workspace and click "Add New Log" to create a tracker. You
             can name it and later rename or delete it as needed.
           </p>
@@ -34,7 +34,7 @@ const Instructions = () => {
           transition={{ duration: 0.5 }}
         >
           <h3 className="item-title">3. Log and Categorize</h3>
-          <p className="pl-2 mb-5">
+          <p className="pl-2 mb-5 text-base">
             Record your finances and assign them to categories. Customize each
             category with colors to make them easy to identify. Use filters and
             search tools for quick navigation.
@@ -47,7 +47,7 @@ const Instructions = () => {
           transition={{ duration: 0.5 }}
         >
           <h3 className="item-title">4. Save Your Logs</h3>
-          <p className="pl-2 mb-5">
+          <p className="pl-2 mb-5 text-base">
             Click the download button to save a copy of your log. This will
             generate a spreadsheet version, including your entries, categories,
             and summary.
