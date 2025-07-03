1fd14b5ea2b215ebbde38b6663d4c8b6fc9eaf1b
marlmunar <marlemmmunar@gmail.com>
2025-07-03 15:20:18 +0800

    Improved the modal wrapper

--- frontend/src/components/ModalWrapper.jsx
+++ frontend/src/components/ModalWrapper.jsx
@@ -1,29 +1,102 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { TbX } from "react-icons/tb";
 import { useDispatch, useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { setModalState } from "../slices/appSlice";
+import AddNewLog from "./Modals/AddNewLog";
+import { AnimatePresence, motion } from "framer-motion";
+import Rename from "./Modals/Rename";
+import Delete from "./Modals/Delete";
 
 const ModalWrapper = () => {
   const dispatch = useDispatch();
-  const { showModal } = useSelector((state) => state.app);
-  const { activeModal } = useSelector((state) => state.app);
-  const { modalData } = useSelector((state) => state.app);
+  const { showModal, activeModal, modalData } = useSelector(
+    (state) => state.app
+  );
+
+  const modals = {
+    addLog: <AddNewLog />,
+    rename: <Rename />,
+    delete: <Delete />,
+  };
+
+  const getModal = () => modals[activeModal] || null;
+
+  const closeModal = () => {
+    dispatch(
+      setModalState({
+        showModal: false,
+        activeModal: "",
+        modalData: {},
+      })
+    );
+  };
+
+  const scrollToRef = useRef(null);
+
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (e.key === "Escape") closeModal();
+    }
+
+    if (showModal) {
+      document.addEventListener("keydown", handleKeyDown);
+      scrollToRef.current.focus();
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [showModal]);
+
+  useEffect(() => {
+    const offset = 200;
+    const element = scrollToRef.current;
+
+    if (element) {
+      const topPosition =
+        element.getBoundingClientRect().top + window.scrollY - offset;
+
+      window.scrollTo({
+        top: topPosition,
+        behavior: "smooth",
+      });
+    }
+  }, [showModal]);
+
   return (
     <div>
-      {showModal ? (
-        <>
-          <div className="mt-[-5px] z-20 flex justify-center items-center absolute h-[calc(100%+5px)] w-full bg-gray-300/75 text-5xl">
-            <div className="sticky top-0">
-              <p>{activeModal}</p>
-              <p>{JSON.stringify(modalData)}</p>
-            </div>
+      {showModal && (
+        <div
+          className="mt-[-5px] z-20 absolute h-[calc(100%+5px)] w-full bg-gray-300/75 outline-none"
+          onClick={closeModal}
+          ref={scrollToRef}
+          tabIndex={-1}
+        >
+          <div className="relative w-full h-full flex justify-center items-start">
+            <AnimatePresence mode="wait">
+              <motion.div
+                initial={{ opacity: 0, y: 100 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: 100 }}
+                transition={{ duration: 0.3 }}
+                className="bg-white rounded p-4 relative top-[20%] min-w-[50%]"
+                onClick={(e) => e.stopPropagation()}
+              >
+                {getModal()}
+                <button
+                  className="absolute top-4 right-4 text-2xl log-tool-button min-h-12 min-w-12"
+                  onClick={closeModal}
+                >
+                  <TbX />
+                </button>
+              </motion.div>
+            </AnimatePresence>
           </div>
-          <Outlet />
-        </>
-      ) : (
-        <Outlet />
+        </div>
       )}
+
+      <Outlet />
     </div>
   );
 };
---
+++ frontend/src/components/Modals/AddNewLog.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Modal from "../Modal";
+
+const AddNewLog = () => {
+  return <>Add New Log</>;
+};
+
+export default AddNewLog;
