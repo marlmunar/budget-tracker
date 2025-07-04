4331cc433f3a949cb9867fb5998aa4353de7a818
marlmunar <marlemmmunar@gmail.com>
2025-07-04 14:25:51 +0800

    Improved the delete log modal UI

--- frontend/src/components/Modals/Delete.jsx
+++ frontend/src/components/Modals/Delete.jsx
@@ -7,11 +7,13 @@ const Delete = ({ resource }) => {
 
   return (
     <form className="flex flex-col gap-2 p-2" onSubmit={handleSubmit}>
-      <h3 className="text-xl font-semibold">Deleting {resource.name} </h3>
-      <p>Do you want to delete this {resource.type}?</p>
-      <div className="self-end">
-        <button className="shadow rounded w-18 py-2 mr-2">Confirm</button>
-        <button className="shadow rounded w-18 py-2">Cancel</button>
+      <h3 className="text-xl font-semibold">Delete {resource.name} </h3>
+      <p className="modal-input-container">
+        Do you want to delete this {resource.type}?
+      </p>
+      <div className="self-end flex gap-2">
+        <button className="modal-delete-button">Confirm</button>
+        <button className="modal-action-button">Cancel</button>
       </div>
     </form>
   );
