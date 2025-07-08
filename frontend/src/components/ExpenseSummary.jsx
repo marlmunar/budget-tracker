908ee033be25c56bf22e4791072c5a6d78d39cea
marlmunar <marlemmmunar@gmail.com>
2025-07-08 11:06:56 +0800

    Made UI changes on the summary items for log screen

--- frontend/src/components/ExpenseSummary.jsx
+++ frontend/src/components/ExpenseSummary.jsx
@@ -5,6 +5,7 @@ const ExpenseSummary = ({ entries }) => {
   const uniqueCategories = [
     ...new Set(entries.map((cat) => cat.category.name)),
   ];
+
   const sumPerCategory = uniqueCategories
     .map((cat) => ({
       category: cat,
@@ -13,20 +14,28 @@ const ExpenseSummary = ({ entries }) => {
       }, 0),
     }))
     .sort((a, b) => b.amount - a.amount);
+
+  const formatNumber = (value) => {
+    if (!value) return "0";
+
+    const [integerPart, decimalPart] = value.toString().split(".");
+
+    const formattedInt = parseInt(integerPart, 10).toLocaleString("en-US");
+
+    const trimmedDecimal = decimalPart ? decimalPart.slice(0, 4) : "";
+
+    return trimmedDecimal ? `${formattedInt}.${trimmedDecimal}` : formattedInt;
+  };
+
   return (
     <section className="log-section-container">
-      <h2 className="log-section-header">Expense Summary</h2>
+      <div className="log-section-header">
+        <h2 className="h-10 flex items-center">Expense Summary</h2>
+      </div>
 
       {entries.length > 0 ? (
-        <>
-          <div className="flex justify-between px-2 pt-2 font-semibold text-lg md:text-xl">
-            <h3>{`Entries: ${entries.length}`}</h3>
-            <h3>{`Total: ${entries.reduce(
-              (sum, entry) => +entry.amount + sum,
-              0
-            )}`}</h3>
-          </div>
-          <div className="flex text-gray-600 justify-between px-2 pb-1 font-semibold text-base md:text-lg">
+        <div className="p-2">
+          <div className="flex bg-slate-200 p-2 rounded text-gray-800 justify-between font-semibold">
             <h3>Category</h3>
             <h3>Subtotal</h3>
           </div>
@@ -34,10 +43,23 @@ const ExpenseSummary = ({ entries }) => {
             <ExpenseSummaryItem
               key={index}
               category={item.category}
-              amount={item.amount}
+              amount={formatNumber(item.amount)}
             />
           ))}
-        </>
+          <div className="flex bg-slate-200 p-2 rounded text-gray-800 justify-between font-semibold mb-2">
+            <h3>Total</h3>
+          </div>
+          <div className="flex justify-between shadow p-2 rounded">
+            <p className="font-semibold">
+              {`${entries.length} ${entries.length > 1 ? "entries" : "entry"}`}
+            </p>
+            <p>
+              {formatNumber(
+                entries.reduce((sum, entry) => +entry.amount + sum, 0)
+              )}
+            </p>
+          </div>
+        </div>
       ) : (
         <NoRecords />
       )}
--- frontend/src/components/ExpenseSummaryItem.jsx
+++ frontend/src/components/ExpenseSummaryItem.jsx
@@ -1,10 +1,10 @@
 const ExpenseSummaryItem = ({ category, amount }) => {
   return (
-    <div className="my-2 p-2 px-4 flex justify-between bg-white shadow rounded hover:bg-slate-200/50 transition-all duration-300">
-      <p className="text-base md:tetx-lg font-semibold col-span-2">
+    <div className="my-2 p-2 px-4 flex flex-col justify-between bg-white shadow rounded hover:bg-slate-200/50 transition-all duration-300">
+      <p className="text-base md:tetx-lg font-semibold col-span-2 py-2">
         {category}
       </p>
-      <p>{amount}</p>
+      <p className="self-end">{amount}</p>
     </div>
   );
 };
