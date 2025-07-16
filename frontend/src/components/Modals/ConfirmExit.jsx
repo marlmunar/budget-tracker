b8db3b5a8682549886931957f3c140b30d50aa75
marlmunar <marlemmmunar@gmail.com>
2025-07-16 12:44:06 +0800

    Adjusted the confirm exit modal for sm screens

--- frontend/src/components/Modals/ConfirmExit.jsx
+++ frontend/src/components/Modals/ConfirmExit.jsx
@@ -3,9 +3,11 @@ import useNavigationBlocker from "../../hooks/useNavigationBlocker";
 
 const ConfirmExit = ({ closeModal, confirmNavigation }) => {
   return (
-    <form className="flex flex-col gap-2 p-2">
-      <h3 className="text-xl font-semibold">Confirm Exit</h3>
-      <p className="modal-input-container">
+    <form className="flex flex-col gap-2 p-2 md:p-0 min-w-72">
+      <h3 className="text-base md:text-xl font-semibold flex items-center gap-1">
+        Confirm Exit
+      </h3>
+      <p className="text-sm md:text-base max-w-[20ch] modal-input-container">
         You are trying to leave a log with unsaved changes.
       </p>
       <div className="self-end flex gap-2">
