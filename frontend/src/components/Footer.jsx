825861be5db931efc7305eabf927b145060f67ba
marlmunar <marlemmmunar@gmail.com>
2025-08-07 11:22:37 +0800

    Added dark mode for footer components

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,8 @@
 const Footer = ({ bg = "bg-[#FFE779]" }) => {
   return (
-    <footer className={`mt-auto p-2 ${bg}`}>
+    <footer
+      className={`mt-auto p-2 ${bg} dark:bg-[#2b2d2f] dark:text-[#f0f0f0]`}
+    >
       <div className="lg:px-10 text-[0.55rem] md:text-xs">
         <div className="container px-5 mx-auto md:px-10 min-h-4 md:min-h-10 flex items-center justify-between">
           <p>
--- frontend/src/components/PageWrapper.jsx
+++ frontend/src/components/PageWrapper.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const PageWrapper = () => {
   return (
-    <div className="flex-1 container w-full h-full py-2 lg:px-10 md:px-2 mx-auto grow dark:bg-[#232323] dark:text-[#f8fdff]">
+    <div className="flex-1 container w-full h-full py-2 lg:px-10 md:px-2 mx-auto grow dark:bg-[#232323] dark:text-[#f0f0f0]">
       <Outlet />
     </div>
   );
