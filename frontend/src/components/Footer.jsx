21170a1dab4ec89f9337232c1a9ee78b3dc6b0d3
marlmunar <marlemmmunar@gmail.com>
2025-08-08 12:36:18 +0800

    Improved footer dark mode appearance

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,10 +7,14 @@ const Footer = ({ bg = "bg-[#FFE779]" }) => {
         <div className="container px-5 mx-auto md:px-10 min-h-4 md:min-h-10 flex items-center justify-between">
           <p>
             Copyright &#169; 2025{" "}
-            <span className="font-semibold"> Budgetarians' Log</span>
+            <span className="font-semibold dark:font-normal">
+              {" "}
+              Budgetarians' Log
+            </span>
           </p>
           <p>
-            Project by <span className="font-semibold">marl.dev</span>
+            Project by{" "}
+            <span className="font-semibold dark:font-normal">marl.dev</span>
           </p>
         </div>
       </div>
