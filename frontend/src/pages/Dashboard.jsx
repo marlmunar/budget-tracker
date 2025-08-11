5077903bad3cb2648949e789ea582cb09b3779c1
marlmunar <marlemmmunar@gmail.com>
2025-08-11 10:49:18 +0800

    Added a month selector

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import OutsideClick from "../components/OutsideClick";
+import { Link } from "react-router-dom";
+import { TbCaretDownFilled, TbCaretUpFilled } from "react-icons/tb";
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const Dashboard = () => {
+  const [isSelectingMonth, setIsSelectingMonth] = useState(false);
+  const [month, setMonth] = useState("");
+  const [isSelectingDay, setIsSelectingDay] = useState(false);
+
   return (
     <>
       <title>Budgetarians' Log - Dashboard</title>
       <main className="h-full flex">
         <section className="w-full h-full border">
-          <div>
-            <div></div>
+          <div className="flex gap-1 max-w-48 p-1 bg-gray-200 rounded">
+            <p>Month:</p>
+            <div className="bg-gray-400 w-full px-1 rounded relative">
+              <div
+                className="flex justify-between *:pointer-events-none"
+                onClick={() => setIsSelectingMonth((prev) => !prev)}
+                data-id="selectMonth"
+              >
+                <p>{month ? month : "Select Month"}</p>
+                <button>
+                  {isSelectingMonth ? (
+                    <TbCaretUpFilled />
+                  ) : (
+                    <TbCaretDownFilled />
+                  )}
+                </button>
+              </div>
+              {isSelectingMonth && (
+                <OutsideClick
+                  onOutsideClick={() => setIsSelectingMonth(false)}
+                  id="selectMonth"
+                >
+                  <menu className="absolute top-6 right-0 w-full border space-y-1 px-1">
+                    {months.map((month) => (
+                      <li
+                        key={month}
+                        onClick={() => {
+                          setMonth(month);
+                          setIsSelectingMonth(false);
+                        }}
+                      >
+                        {month}
+                      </li>
+                    ))}
+                  </menu>
+                </OutsideClick>
+              )}
+            </div>
           </div>
         </section>
         <section className="w-full h-full border"></section>
