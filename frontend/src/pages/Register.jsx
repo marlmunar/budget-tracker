2349f443b5713b957a680e140daa457464696b59
marlmunar <marlemmmunar@gmail.com>
2025-08-20 17:09:24 +0800

    Fixed minor appearance bug in register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -262,7 +262,7 @@ const Register = () => {
 
           <div
             className="p-1 rounded h-full row-span-2 md:flex flex-col justify-end 
-            md:bg-amber-300/75 md:absolute right-0 top-0 md:w-[30%] 
+            md:bg-amber-300/75 md:absolute left-0 top-0 md:w-[30%] 
             lg:py-6 md:p-4 md:shadow-[2px_0_6px_rgba(0,0,0,0.1)]
             md:dark:bg-[#ffe368] md:dark:text-[#282828]"
           >
